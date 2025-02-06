6dec6931367c102763d2d2a54a7728c351fc9642
Joao Tentis <101594270+jtentis@users.noreply.github.com>
2025-02-06 11:39:38 -0300

    Primeiro commit

---
+++ src/main/java/com/teste_iniflex/teste_iniflex/TesteIniflexApplication.java
@@ -0,0 +1,13 @@
+package com.teste_iniflex.teste_iniflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteIniflexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteIniflexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teste_iniflex/teste_iniflex/TesteIniflexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste_iniflex.teste_iniflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteIniflexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
