3533377fb1578b0979943832afad3bae446e53dd
Joao Tentis <101594270+jtentis@users.noreply.github.com>
2025-02-07 14:37:08 -0300

    Fix: ajeitando ordem dos endpoints no swagger

--- src/main/java/com/teste_iniflex/teste_iniflex/config/SwaggerConfig.java
+++ src/main/java/com/teste_iniflex/teste_iniflex/config/SwaggerConfig.java
@@ -4,8 +4,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -15,6 +18,12 @@ public class SwaggerConfig {
                 .title("API para o teste técnico da Projedata")
                 .version("1.0")
                 .description("Documentação da API com Swagger"))
+                .tags(
+                        List.of(
+                                new Tag().name("Autenticação").description("Endpoints de autenticação"),
+                                new Tag().name("Funcionários").description("Endpoints de gerenciamento de funcionários")
+                        )
+                )
                 .addSecurityItem(new SecurityRequirement().addList("JWT"))
                 .components(new io.swagger.v3.oas.models.Components()
                         .addSecuritySchemes("JWT", new SecurityScheme()
--- src/main/java/com/teste_iniflex/teste_iniflex/controller/AuthController.java
+++ src/main/java/com/teste_iniflex/teste_iniflex/controller/AuthController.java
@@ -2,9 +2,12 @@ package com.teste_iniflex.teste_iniflex.controller;
 
 import com.teste_iniflex.teste_iniflex.dto.AuthDTO;
 import com.teste_iniflex.teste_iniflex.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Tag(name = "Autenticação")
 @RequestMapping("/auth")
 public class AuthController {
 
@@ -15,11 +18,13 @@ public class AuthController {
     }
 
     @PostMapping("/register")
+    @Operation(summary = "Registrar usuário")
     public String register(@RequestBody AuthDTO request) {
         return service.register(request);
     }
 
     @PostMapping("/login")
+    @Operation(summary = "Autenticar usuário")
     public String login(@RequestBody AuthDTO request) {
         return service.login(request);
     }
